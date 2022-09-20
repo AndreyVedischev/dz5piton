1dd50bcf5d779a9fb4d29255d6322c61d9dcbe0c
Andrey <vedischev76@gmail.com>
2022-09-20 21:14:57 +0500

    apdate task1

--- task1.py
+++ task1.py
@@ -1,1 +1,18 @@
 # Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+
+
+# my_str = 'Мы неабв очень любим Питон иабв Джавуабв'.split()
+# print(' '.join([word for word in my_str if 'абв' not in word]))
+
+
+my_str ='Куда уехал цирк он был еще вчера И ветер не успел со стен сорвать афиши'.split()
+
+new_str = []
+fragment = ('а' or 'б' or 'в')
+for word in my_str:
+    if fragment not in word:
+        new_str.append(word)
+
+print(' '.join(new_str))
+
+
